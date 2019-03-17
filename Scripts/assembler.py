e3d9fff7790d9c1a1f83bb2dc90dbeb11017a11f
Gabriel Russell <TheBlargbla@gmail.com>
2019-03-17 14:18:45 -0400

    Assembler Changes
    Supports Input / Output now. Formatting docs and more changes will come.
    
    Goals: Error Detection / Messages, Warnings, variable Hex/Dec/Bin typing, Functions.

--- Scripts/assembler.py
+++ Scripts/assembler.py
@@ -4,14 +4,12 @@
 # Only part way done. Will probably clean up later.
 # The only current documentation on the assembly language is on my computer,
 # More information will be released later.
-
-command = input("Enter in a command ")
-command = command.split()
-
-# Set some temporary global values
-register = 0 
-operation = 0
-operand = 0
+print("This program is an assembler for the Nomad CPU.")
+print("The standard naming conventions for Assembly Files is the 'Nomad Assembley File, or .naf.'")
+print("The standard naming conventions for Ouput Files is the 'Read Only Memory,' or .rom.'")
+inFile = open(input("Input Filename: "), 'r')
+outFile = open(input("Output Filename: "), 'w')
+outFile.write("2.0 raw\n")
 
 # Translation of Register Values.
 registerKeys = {
@@ -19,7 +17,7 @@ registerKeys = {
         "x0": "2","x1": "3","x2": "4","x3": "5","x4": "6","x5": "7","x6": "8",
         "s0": "9","s1": "A","s2": "B",
         "i0": "C","i1": "D","i2": "E","i3": "F"
-    }
+        }
 # Translation of Operation Values
 operationKeys = {
     "nop": "0",
@@ -29,33 +27,43 @@ operationKeys = {
     "mov": "9", "lmr": "A", "lrm": "B",
     "jeq": "C", "jnq": "D", "jgt": "E", "jlt": "F"
     }
+   
+# Set some temporary global values
+register = 0 
+operation = 0
+operand = 0
 
-operation = operationKeys[command[0]]
+for line in inFile:
+    command = line.split()
+
+    operation = operationKeys[command[0]]
     
-if command[0] in ("nop"):
-    operand = "00"
-    register = "0"
-elif command[0] in ("add, sub", "and", "not", "or", "xor"):
-    register = command[1]
-    operand = "00"
-elif command[0] in ("lsl", "lsr"):
-    # TO DO:
-    # ADD PROTECTIONS, 3 BIT MAXIMUM FOR SHIFTS. IF OR > 7, REJECT CODE
-    register = registerKeys[command[1]]
-    operand = "0" + str(hex(int(command[2]))[2:])
-elif command[0] in "lmr":
-    register = registerKeys[command[1]]
-    operand = command[2][2:]
-elif command[0] in "lrm":
-    register = registerKeys[command[2]]
-    operand = command[1][2:]
-elif command[0] in "mov":
-    register = registerKeys[command[1]]
-    operand = hex(int(command[2]))[2:]
-elif command[0] in ("jeq", "jnq", "jgt", "jlt"):
-    operand = hex(int(command[1]))[2:]
+    if command[0] in ("nop"):
+        operand = "00"
+        register = "0"
+    elif command[0] in ("add, sub", "and", "not", "or", "xor"):
+        register = registerKeys[command[1]]
+        operand = "00"
+    elif command[0] in ("lsl", "lsr"):
+        # TO DO:
+        # ADD PROTECTIONS, 3 BIT MAXIMUM FOR SHIFTS. IF OR > 7, REJECT CODE
+        register = registerKeys[command[1]]
+        operand = "0" + str(hex(int(command[2]))[2:])
+    elif command[0] in "lmr":
+        register = registerKeys[command[1]]
+        operand = command[2][2:]
+    elif command[0] in "lrm":
+        register = registerKeys[command[2]]
+        operand = command[1][2:]
+    elif command[0] in "mov":
+        register = registerKeys[command[1]]
+        operand = hex(int(command[2]))[2:]
+    elif command[0] in ("jeq", "jnq", "jgt", "jlt"):
+        operand = hex(int(command[1]))[2:]
 
-print(str(operation) + str(register) + str(operand))
+    print(str(operation) + str(register) + str(operand))
+    outFile.write(str(operation) + str(register) + str(operand) + "\n")
+    
 
 # Comment this out for now - just some special formulas to copy
 # register = registerKeys[command[1]]
